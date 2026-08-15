d4eb7b9def644c1d817136b17efa93f00babaac7
jasonbai <15810683857@163.com>
2026-08-15 17:02:26 +0800

    build and release

--- docs/lib/baiduStatics.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d2040d6d23d2430bd3d998faa5ef8bca";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
